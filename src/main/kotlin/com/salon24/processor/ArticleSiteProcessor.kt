26089cc49f77326a4daeef2de167c98db1cf15fd
msieniawski <mikolajsieniawskii@gmail.com>
2019-05-14 23:05:50 +0200

    Add logging utils

--- src/main/kotlin/com/salon24/processor/ArticleSiteProcessor.kt
+++ src/main/kotlin/com/salon24/processor/ArticleSiteProcessor.kt
@@ -1,12 +1,14 @@
 package com.salon24.processor
 
 import com.salon24.crawler.SiteInfo
+import com.salon24.utils.logger
 import org.springframework.stereotype.Component
 
 @Component
 class ArticleSiteProcessor : SiteProcessor {
+    private val log by logger()
 
     override fun process(site: SiteInfo) {
-
-    }
+        log.debug("Processing an article: ${site.url}")
+    }it a
 }
---
+++ src/main/kotlin/com/salon24/utils/LoggingUtils.kt
@@ -0,0 +1,10 @@
+package com.salon24.utils
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+fun <T : Any> T.logger(): Lazy<Logger> = logger(javaClass)
+
+fun <T : Any> T.logger(clazz: Class<*>): Lazy<Logger> = logger(clazz.name)
+
+fun <T : Any> T.logger(name: String): Lazy<Logger> = lazy { LoggerFactory.getLogger(name) }
